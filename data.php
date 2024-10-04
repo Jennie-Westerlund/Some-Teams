88359eea7f4dfe0485fd29094491b543f66acc7c
Jennie-Westerlund <jmn.westerlund@gmail.com>
2024-10-04 12:48:08 +0200

    Imported data file

---
+++ data.php
@@ -0,0 +1,132 @@
+<?php
+
+$teams = [
+    'Barcelona' => [
+        'league' => 'Primera División (Spain)',
+        'uefa-coefficient-ranking' => 1,
+        'city' => 'Barcelona',
+        'url' => 'https://www.fcbarcelona.com/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg',
+        'group' => 'D'
+    ],
+    'Chelsea' => [
+        'league' => 'Women\’s Super League (England)',
+        'uefa-coefficient-ranking' => 6,
+        'city' => 'London',
+        'url' => 'https://www.chelseafc.com/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/en/c/cc/Chelsea_FC.svg',
+        'group' => 'B'
+    ],
+    'Wolfsburg' => [
+        'league' => 'Frauen-Bundesliga (Germany)',
+        'uefa-coefficient-ranking' => 4,
+        'city' => 'Wolfsburg',
+        'url' => 'https://www.vfl-wolfsburg.de/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/VfL_Wolfsburg_Logo.svg/840px-VfL_Wolfsburg_Logo.svg.png?20150906163520',
+        'group' => 'A'
+    ],
+    'Roma' => [
+        'league' => 'Serie A (Italy)',
+        'uefa-coefficient-ranking' => 17,
+        'city' => 'Rome',
+        'url' => 'https://www.asroma.com/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/en/f/f7/AS_Roma_logo_%282017%29.svg',
+        'group' => 'A'
+    ],
+    'Real Madrid' => [
+        'league' => 'Primera División (Spain)',
+        'uefa-coefficient-ranking' => 8,
+        'city' => 'Madrid',
+        'url' => 'https://www.realmadrid.com/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/en/5/56/Real_Madrid_CF.svg',
+        'group' => 'B'
+    ],
+    'Bayern Munich' => [
+        'league' => 'Frauen-Bundesliga (Germany)',
+        'uefa-coefficient-ranking' => 5,
+        'city' => 'Munich',
+        'url' => 'https://fcbayern.com/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/FC_Bayern_M%C3%BCnchen_logo_%282017%29.svg/1200px-FC_Bayern_M%C3%BCnchen_logo_%282017%29.svg.png',
+        'group' => 'C'
+    ],
+    'Paris Saint-Germain' => [
+        'league' => 'Division 1 Féminine (France)',
+        'uefa-coefficient-ranking' => 2,
+        'city' => 'Paris',
+        'url' => 'https://en.psg.fr/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/en/a/a7/Paris_Saint-Germain_F.C..svg',
+        'group' => 'N/A'
+    ],
+    'Arsenal' => [
+        'league' => 'Women\’s Super League (England)',
+        'uefa-coefficient-ranking' => 7,
+        'city' => 'London',
+        'url' => 'https://www.arsenal.com/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/en/5/53/Arsenal_FC.svg',
+        'group' => 'C'
+    ],
+    'Lyon' => [
+        'league' => 'Division 1 Féminine (France)',
+        'uefa-coefficient-ranking' => 3,
+        'city' => 'Lyon',
+        'url' => 'https://www.ol.fr/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/en/thumb/1/1c/Olympique_Lyonnais_logo.svg/1200px-Olympique_Lyonnais_logo.svg.png',
+        'group' => 'A'
+    ],
+    'Hammarby' => [
+        'league' => 'Damallsvenskan (Sweden)',
+        'uefa-coefficient-ranking' => 'Not ranked',
+        'city' => 'Stockholm',
+        'url' => 'https://www.hammarbyfotboll.se/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/en/thumb/0/0a/Hammarby_IF_logo.svg/1200px-Hammarby_IF_logo.svg.png',
+        'group' => 'D'
+    ],
+    'Juventus' => [
+        'league' => 'Serie A (Italy)',
+        'uefa-coefficient-ranking' => 10,
+        'city' => 'Turin',
+        'url' => 'https://www.juventus.com/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Juventus_Logo.png/1200px-Juventus_Logo.png',
+        'group' => 'C'
+    ],
+    'Twente' => [
+        'league' => 'Eredivisie (Netherlands)',
+        'uefa-coefficient-ranking' => 31,
+        'city' => 'Enschede',
+        'url' => 'https://www.fctwente.nl/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e3/FC_Twente.svg/1200px-FC_Twente.svg.png',
+        'group' => 'B'
+    ],
+    'Galatasaray' => [
+        'league' => 'Turkish Women\’s Super League',
+        'uefa-coefficient-ranking' => 'Not ranked',
+        'city' => 'Istanbul',
+        'url' => 'https://www.galatasaray.org/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Galatasaray_Sports_Club_Logo.png/822px-Galatasaray_Sports_Club_Logo.png',
+        'group' => 'A'
+    ],
+    'Vålerenga' => [
+        'league' => 'Toppserien (Norway)',
+        'uefa-coefficient-ranking' => 38,
+        'city' => 'Oslo',
+        'url' => 'https://www.vif-fotball.no/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/V%C3%A5lerenga_Oslo_logo.svg/1280px-V%C3%A5lerenga_Oslo_logo.svg.png',
+        'group' => 'C'
+    ],
+    'SKN St. Pölten' => [
+        'league' => 'ÖFB-Frauenliga (Austria)',
+        'uefa-coefficient-ranking' => 26,
+        'city' => 'St. Pölten',
+        'url' => 'https://www.skn-frauen.at/',
+        'logo' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkiSy4BkhZp1RgOL8cKly0rIU2uSXshWGSzQ&s',
+        'group' => 'D'
+    ],
+    'Manchester City' => [
+        'league' => 'Women\’s Super League (England)',
+        'uefa-coefficient-ranking' => 9,
+        'city' => 'Manchester',
+        'url' => 'https://www.mancity.com/',
+        'logo' => 'https://upload.wikimedia.org/wikipedia/en/e/eb/Manchester_City_FC_badge.svg',
+        'group' => 'D'
+    ],
+];
