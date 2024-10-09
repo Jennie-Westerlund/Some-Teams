181a7e2aa287cfe6b9d02a961d781e51e6d53b1c
Jennie-Westerlund <jmn.westerlund@gmail.com>
2024-10-09 12:51:56 +0200

    Fixed the link to the image on the about

--- about.php
+++ about.php
@@ -6,7 +6,7 @@
         <P>My name is Jennie Westerlund and I have been playing football for 11 years. My main position was wing back but I also played centre back if needed. I started off in Balltorps FF F97 at 7 years old. Later the F97 team merged with Balltorps FF women’s A-team. We were a bit too young to play with the adults but because of a lack of players they had no choice. Sadly I had to quit at 17 years old due to complications with my calves. But the passion for women’s football remains. Today I mainly follow the Swedish national team and their careers outside of the national team. Some of my favourite players are Johanna Rytting Kaneryd, Kosovare Asllani, Magdalena Eriksson, Fridolina Rolfö and Zecira Musovic.
         </P>
     </div>
-    <img class="jennie-img" src="/Jennie-1.jpg" alt="Image of Jennie playing football from behind">
+    <img class="jennie-img" src="Jennie-1.jpg" alt="Image of Jennie playing football from behind">
 </div>
 
 <?php require __DIR__ . "/footer.php"; ?>
